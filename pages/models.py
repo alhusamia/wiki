2bbc59230e10b7e576672e4a02f8941eb22639b8
PC <fdhfdh>
2020-02-13 10:32:58 +0200

    adding wiki

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    last_update = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse('page-detail', args=[str(self.id)])
